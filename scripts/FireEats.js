8252c7fdfaddaed2329c9a19661e5fafca230eaa
Erik Haddad <erikhaddad@gmail.com>
2018-01-23 22:59:15 -0800

    added PWA support changed Token container positioning corrected dialog errors

--- scripts/FireEats.js
+++ scripts/FireEats.js
@@ -242,4 +242,10 @@ FireEats.prototype.data = {
 
 window.onload = () => {
     window.app = new FireEats();
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function() { console.log('Service Worker Registered'); });
+    }
 };
